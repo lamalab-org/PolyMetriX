bb50943fc07aa3601915803112e8ffe3197d0eee
sreekanth221998 <sreekanthkuncha@gmail.com>
2025-02-25 10:24:08 +0100

    make label_name configuarable

--- src/polymetrix/splitters/splitters.py
+++ src/polymetrix/splitters/splitters.py
@@ -12,6 +12,7 @@ class TgSplitter(BaseSplitter):
         self,
         ds: AbstractDataset,
         tg_q: Optional[Collection[float]] = None,
+        label_name: str = "labels.Exp_Tg(K)",
         shuffle: bool = True,
         random_state: Optional[Union[int, np.random.RandomState]] = None,
         **kwargs,
@@ -21,16 +22,18 @@ class TgSplitter(BaseSplitter):
         Args:
             ds: Dataset to split
             tg_q: Quantiles to bin Tg values into groups
+            label_name: Name of the label to use for splitting
             shuffle: Whether to shuffle the dataset
             random_state: Random state for shuffling
             **kwargs: Additional arguments to pass to BaseSplitter
         """
         self._grouping_q = tg_q
+        self._label_name = label_name
         super().__init__(ds=ds, shuffle=shuffle, random_state=random_state, **kwargs)
 
     def _get_groups(self) -> Collection[int]:
         """Bin Tg values into quantile-based groups"""
         tg_values = self._ds.get_labels(
-            idx=range(len(self._ds)), label_names=["labels.Exp_Tg(K)"]
+            idx=range(len(self._ds)), label_names=[self._label_name]
         ).flatten()
         return quantile_binning(tg_values, self._grouping_q)
